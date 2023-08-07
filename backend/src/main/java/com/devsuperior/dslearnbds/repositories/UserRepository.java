68678958f684cb4058920cc282c31f01b8838794
Suporte-01 <suporte01@ppmike.local>
2023-08-07 13:58:59 -0300

    att

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.devsuperior.dslearnbds.repositories;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearnbds.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
